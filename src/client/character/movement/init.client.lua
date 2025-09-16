cdab2958026c21754ed9a868120c94eba3d65dd1
Griffin-Dalby <griffindalby7@outlook.com>
2025-09-16 10:00:13 -0600

    feat(movement): Movement code structure

---
+++ src/client/character/movement/init.client.lua
@@ -0,0 +1,19 @@
+--[[
+
+    Movement Controller
+
+    Griffin Dalby
+    2025.09.14
+
+    This script will provide a controller for player movement, and
+    camera movements.
+
+--]]
+
+--]] Services
+--]] Modules
+--]] Settings
+--]] Constants
+--]] Variables
+--]] Functions
+--]] Script
